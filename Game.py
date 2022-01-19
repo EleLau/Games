0169b8d78eb53136180dc354bd928497b62c4212
David-Games <46458974+David-Games@users.noreply.github.com>
2022-01-19 21:58:46 +0100

    Kommentare eingefügt

--- Game.py
+++ Game.py
@@ -1,34 +1,36 @@
 import pygame 
 
-
-screen = pygame.display.set_mode((640,240))
+# SETUP von der PiGame Enginee
+screen = pygame.display.set_mode((640,240)) #Fenster erstellen
+#Fenstertitel
 caption = "Viel gewinnt"
 pygame.display.set_caption(caption)
+# Hintergrundfarben
 red = (255,0,0)
 turqies = (0, 255, 255)
 screen.fill(turqies)
 background = turqies
 pygame.init()
 
+#variablen 
+running = True # laufen des Games solange True
 
-running = True
-
+# Aktive Schleife des Pygames, die den Inhalt updatet
 while running:
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    pygame.display.update() # Jeden Zyklus das angezeigte Bild aktualisieren
+    # Key press erkennen und ausführen Aktionen resultierender Aktionen 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: #möglichkeit zum beenden des Games
+            running = False # Schleife beendne
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 background = red
                 
             elif event.key == pygame.K_t:
                 background = turqies
-            screen.fill(background)
+            screen.fill(background) #neuen Hintergrund entsprechend der gedrückten Taste
                 
-
-
-    
+ # Beenden des Games (nach austreten aus der aaktiven Loop )  
 pygame.quit()
 
 
